4f70527f3c69dd4e7deebd0f28ba4c37039fa83c
Hossam Nassar <hussamnassar96@gmail.com>
2025-05-26 00:33:51 +0300

    Added file no.8

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0], 10);
+
+if (isNaN(size) || size <= 0) {
+  console.log("Missing size");
+} else {
+  const row = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(row);
+  }
+}
